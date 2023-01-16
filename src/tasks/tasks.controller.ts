da3ba14bcd6b626442a4b35919e8666198dc1b1f
Tristan <tristan@entrostat.com>
2023-01-16 16:33:37 +0200

    Adding filter methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,22 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from "./task.model";
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    // If we have any filters defined, call tasksService.getTasksWithFilters
+    // Otherwise, just get all tasks
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWIthFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
     // Doesn't have to be the same name, but it might be helpful to keep tack of everything
     return this.tasksService.getAllTasks();
   }
