9f9a02203bd50452aa45b097ca6f0d7d1f874e0b
Tristan <tristan@entrostat.com>
2023-01-16 11:57:54 +0200

    Create a request

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    // Doesn't have to be the same name, but it might be helpful to keep tack of everything
+    return this.tasksService.getAllTasks();
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,5 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TasksService {
-
+  private tasks = []; // private to limit room for error, empty array of tasks.
+  getAllTasks() { // public by default
+    return this.tasks;
+  }
 }
