bd924e8eedef377c209fb6ca506587287e08f431
Tristan <tristan@entrostat.com>
2023-01-18 08:28:18 +0200

    Secure sign-in and password hashing

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,13 +5,22 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   // deals with database
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    // want to now hash the password with bcrypt.
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log('salt: ', salt);
+    console.log('hashedPassword: ', hashedPassword);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
