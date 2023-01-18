2c7cb178d12424559b66e17cbd16e4428bd49205
Tristan <tristan@entrostat.com>
2023-01-18 11:06:32 +0200

    Remove User being shown in response

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // whenever we need validation we use this. // Add { forbidUnknownValues: false }, if no decorators in dto
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3000);
 }
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { User } from '../auth/user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task {
@@ -18,5 +19,6 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true }) // whenever printing that property, we want to exclude it frm being shown.
   user: User;
 }
