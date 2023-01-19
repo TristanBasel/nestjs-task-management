2b9ff3a34fbc7b389dcebaf2e0d6e0b3cb19b692
Tristan <tristan@entrostat.com>
2023-01-19 07:10:28 +0200

    TypeORM Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,30 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.dev`],// ${process.env.STAGE}`], // this STAGE, we add to package.json
+      // load: [configuration],
+    }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true, // define entities which get translated, we autoload these.
-      synchronize: true, // always keeps the db schema in sync, an advanced feature.
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: true, // define entities which get translated, we autoload these.
+          synchronize: true, // always keeps the db schema in sync, an advanced feature.
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+        };
+      },
     }),
     AuthModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   // instantiate Logger.
@@ -15,5 +16,8 @@ async function bootstrap() {
   const port = 3000;
   await app.listen(port);
   logger.log(`Application is listening on port ${port}.`);
+
+  // const configService = app.get(ConfigService);
+  // await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
